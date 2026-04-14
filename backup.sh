74845fdcece1c5f5ca45ce0e5ec0832bfecb2f8d
gvrdc <agaugu0101@gmail.com>
2026-04-14 07:30:58 +0000

    improve backup script: add auto folder creation and old backup cleanup

--- backup.sh
+++ backup.sh
@@ -3,9 +3,12 @@
 
 DATE=$(date +%Y-%m-%d_%H-%M)
 
+mkdir -p ~/minecraft_backups
+
 
 cp -r ~/minecraft/data/world ~/minecraft_backups/world_$DATE
 
+find ~/minecraft_backups -mtime +7 -type d -exec rm -rf {} +
 
 echo "Backup done: world_$DATE"
 
